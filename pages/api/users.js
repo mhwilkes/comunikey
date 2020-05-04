61fc72897a3cf7e84810a508cadb82f488ff52b4
Mike <mhwilkes@gmail.com>
2020-05-04 11:46:32 -0400

    working on editing template to conform to user standard

--- pages/api/users.js
+++ pages/api/users.js
@@ -10,13 +10,13 @@ const handler = nextConnect();
 handler.use(middleware);
 
 handler.post(async (req, res) => {
-    const {name, password} = req.body;
+    const {first_name, last_name, password, contact} = req.body;
     const email = normalizeEmail(req.body.email);
     if (!isEmail(email)) {
         res.status(400).send('The email you entered is invalid.');
         return;
     }
-    if (!password || !name) {
+    if (!password || !first_name || !last_name) {
         res.status(400).send('Missing field(s)');
         return;
     }
@@ -26,7 +26,7 @@ handler.post(async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await req.db
         .collection('users')
-        .insertOne({email, password: hashedPassword, name})
+        .insertOne({email, password: hashedPassword, first_name, last_name, contact})
         .then(({ops}) => ops[0]);
     req.logIn(user, (err) => {
         if (err) throw err;
