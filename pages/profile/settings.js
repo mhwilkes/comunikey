a25d83797dee9dfdf38f954cee10c0cc3929071d
Mike <mhwilkes@gmail.com>
2020-06-11 22:31:15 -0400

    form styling in progress

--- pages/profile/settings.js
+++ pages/profile/settings.js
@@ -1,18 +1,37 @@
 import React, {useState, useEffect} from 'react';
-import Layout from '../../components/layout'
+import Layout from '../../components/layout';
 import {useUser} from "../../lib/hooks";
 import CssBaseline from "@material-ui/core/CssBaseline";
+
+import {makeStyles} from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Button from "@material-ui/core/Button";
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
 
 export default () => {
     const [user, {mutate}] = useUser();
 
-    return(
-        <SettingPage user={user} mutate={mutate}/>
+    return (
+        <SettingPage user={user} mutate={mutate} />
     )
 }
 
 const ProfileSection = ({user, mutate}) => {
 
+    const classes = useStyles();
     const [isUpdating, setIsUpdating] = useState(false);
     const [first_name, setFirstName] = useState(user.first_name);
     const [last_name, setLastName] = useState(user.last_name);
@@ -58,6 +77,10 @@ const ProfileSection = ({user, mutate}) => {
         }
     };
 
+    const handleNewSet = async (e) => {
+
+    }
+
     const handleSubmitPasswordChange = async (e) => {
         e.preventDefault();
         const body = {
@@ -83,10 +106,11 @@ const ProfileSection = ({user, mutate}) => {
     return (
         <Layout user={user} mutate={mutate}>
             <h1>Settings</h1>
-            <CssBaseline/>
+            <CssBaseline />
             <h2>Edit Profile</h2>
             {msg.message ?
                 <p style={{color: msg.isError ? 'red' : '#0070f3', textAlign: 'center'}}>{msg.message}</p> : null}
+
             <form onSubmit={handleSubmit}>
                 <label htmlFor="first_name">
                     First Name
@@ -136,25 +160,83 @@ const ProfileSection = ({user, mutate}) => {
                 <button disabled={isUpdating} type="submit">Save</button>
             </form>
             <form onSubmit={handleSubmitPasswordChange}>
-                <label htmlFor="oldpassword">
-                    Old Password
+                <FormControl className={classes.formControl}>
+                    <InputLabel htmlFor="oldpassword">
+                        Old Password
+                    </InputLabel>
+                    <Input id="oldpassword" name="oldpassword" aria-describedby="oldpasshelptext" />
+                    <FormHelperText id="oldpasshelptext">Please enter your old password here</FormHelperText>
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                    <InputLabel htmlFor="newpassword">
+                        New Password
+                    </InputLabel>
+                    <Input id="newpassword" name="newpassword" aria-describedby="newpasshelptext" />
+                    <FormHelperText id="newpasshelptext">Please enter your new password here</FormHelperText>
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                    <Button type="submit" variant="outlined" color="secondary">Change Password</Button>
+                </FormControl>
+            </form>
+            <form onSubmit={handleNewSet}>
+                <label htmlFor="set-name">
+                    Set Name
                     <input
-                        type="password"
-                        name="oldPassword"
-                        id="oldpassword"
+                        type="text"
+                        name="set-name"
+                        id="set-name"
                         required
                     />
                 </label>
-                <label htmlFor="newpassword">
-                    New Password
+                <label htmlFor="author-name">
+                    Author Name
                     <input
-                        type="password"
-                        name="newPassword"
-                        id="newpassword"
+                        type="text"
+                        name="author-name"
+                        id="author-name"
                         required
                     />
                 </label>
-                <button type="submit">Change Password</button>
+                <label htmlFor="manufacturer-name">
+                    Manufacturer
+                    <select
+                        name="manufacturer-name"
+                        id="manufacturer-name"
+                        required>
+                        <option value={"GMK"}>GMK</option>
+                        <option value={"E-PBT"}>E-PBT</option>
+                        <option value={"JTK"}>JTK</option>
+                    </select>
+                </label>
+                <label htmlFor="profile-name">
+                    Profile Type
+                    <select
+                        name="profile-name"
+                        id="profile-name"
+                        required>
+                        <option value={"Cherry"}>Cherry</option>
+                        <option value={"SA"}>SA</option>
+                        <option value={"DSA"}>DSA</option>
+                        <option value={"XDA"}>XDA</option>
+                        <option value={"MT3"}>MT3</option>
+                        <option value={"KAT"}>KAT</option>
+                        <option value={"KAM"}>KAM</option>
+                        <option value={"OEM"}>OEM</option>
+                        <option value={"MDA"}>MDA</option>
+                        <option value={"DCS"}>DCS</option>
+                    </select>
+                </label>
+                <label htmlFor="profile-name">
+                    Profile Type
+                    <select
+                        name="profile-name"
+                        id="profile-name"
+                        required>
+                        <option value={"MX"}>MX</option>
+                        <option value={"Topre"}>Topre</option>
+                        <option value={"Alps"}>Alps</option>
+                    </select>
+                </label>
             </form>
         </Layout>
     );
@@ -170,7 +252,7 @@ const SettingPage = ({user, mutate}) => {
     }
     return (
         <>
-            <ProfileSection user={user} mutate={mutate}/>
+            <ProfileSection user={user} mutate={mutate} />
         </>
     );
 };
