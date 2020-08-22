67bf65bf7fb700bbb9dda2d3b8c6d01ec60d8bfb
Mike <mhwilkes@gmail.com>
2020-08-22 12:51:24 -0400

    eslint work and creating components to prevent multiple user pulls per refresh

--- pages/api/user/password/index.js
+++ pages/api/user/password/index.js
@@ -6,19 +6,19 @@ const handler = nextConnect();
 handler.use(middleware);
 
 handler.put(async (req, res) => {
-    if (!req.user) {
-        res.json(401).send('you need to be authenticated');
-        return;
-    }
-    const {oldPassword, newPassword} = req.body;
-    if (!(await bcrypt.compare(oldPassword, req.user.password))) {
-        res.status(401).send('The password you has entered is incorrect.');
-    }
-    const password = await bcrypt.hash(newPassword, 10);
-    await req.db
-        .collection('users')
-        .updateOne({_id: req.user._id}, {$set: {password}});
-    res.end('ok');
+  if (!req.user) {
+    res.json(401).send('you need to be authenticated');
+    return;
+  }
+  const { oldPassword, newPassword } = req.body;
+  if (!(await bcrypt.compare(oldPassword, req.user.password))) {
+    res.status(401).send('The password you has entered is incorrect.');
+  }
+  const password = await bcrypt.hash(newPassword, 10);
+  await req.db
+    .collection('users')
+    .updateOne({ _id: req.user._id }, { $set: { password } });
+  res.end('ok');
 });
 
 export default handler;
